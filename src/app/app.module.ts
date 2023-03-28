33cae40ca68c2bb244d6da011370cf6e2034116d
Hernando <hernandoorozco032@gmail.com>
2023-03-28 11:38:19 -0500

    Modificacion endpoints

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,61 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//formularios reactivos
+import {ReactiveFormsModule} from '@angular/forms';
+//tragajar peticiones http
+import { HttpClientModule } from '@angular/common/http';
+
+//para trabajar con las tablas
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
+
+//controles de formularios
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material/core';
+import { MomentDateModule } from '@angular/material-moment-adapter'; 
+
+// tabjar con alerta
+import {MatSnackBarModule} from  '@angular/material/snack-bar';
+// tabjar con iconos
+import {MatIconModule} from '@angular/material/icon';
+// tabjar con Modales
+import {MatDialogModule} from '@angular/material/dialog';
+//trabajar con cuadriculas
+import {MatGridListModule} from '@angular/material/grid-list';
+import { DialogAddEditComponent } from './Dialogs/dialog-add-edit/dialog-add-edit.component';
+import { DialogoDeleteComponent } from './Dialogs/dialogo-delete/dialogo-delete.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogAddEditComponent,
+    DialogoDeleteComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MomentDateModule,
+    MatSnackBarModule,
+    MatIconModule,
+    MatDialogModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
